caaab00f5a285338972887b235fbb1e5ecd8d31f
Saurabh Pranjale <saurabh.pranjale20@gmail.com>
2023-07-02 16:03:54 +0530

    Counter App in Javascript

---
+++ script.js
@@ -0,0 +1,24 @@
+const countValue = document.getElementById('counter');
+
+function increment(){
+    // get the vlue from UI
+    let value = parseInt(countValue.innerText);
+    //Update the value
+    value = value + 1;
+    //set the value onto UI
+    countValue.innerText = value;
+    
+};
+
+
+function decrement(){
+        // get the vlue from UI
+        let value = parseInt(countValue.innerText);
+        //Update the value
+        value = value - 1;
+        //set the value onto UI
+        countValue.innerText = value;
+        if(value < 0){
+            countValue.innerText = 0
+        }
+};
